88d0af1b5cc5702e7dfad42f62bf79b2b2c7a76c
Benoit Marcot <juz4m@hotmail.com>
2016-01-02 15:25:51 +0100

    Redefine default types the Linux way.
    Add typedefs for u32, s32, u16...

--- include/linux/types.h
+++ include/linux/types.h
@@ -1,6 +1,15 @@
 #ifndef LINUX_TYPES_H
 #define LINUX_TYPES_H
 
+typedef unsigned int   u32;
+typedef int            s32;
+
+typedef unsigned short u16;
+typedef short          s16;
+
+typedef unsigned char  u8;
+typedef char           s8;
+
 struct list_head {
     struct list_head *next, *prev;
 };
