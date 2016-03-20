33b31cb75b62bff20baba950ddd4b2f76c4fb27e
Benoit Marcot <juz4m@hotmail.com>
2016-03-20 16:01:14 +0100

    (minor) Add missing header guardings.

--- include/bitmap.h
+++ include/bitmap.h
@@ -1,3 +1,6 @@
+#ifndef BITMAP_H
+#define BITMAP_H
+
 #include "linux/types.h"
 
 #define BITS_IN_U32 (sizeof (u32) * 8)
@@ -18,3 +21,5 @@ static inline unsigned bitmap_get(u32 *bm, unsigned ix)
 }
 
 unsigned bitmap_count(u32 *bm, unsigned len);
+
+#endif /* !BITMAP_H */
