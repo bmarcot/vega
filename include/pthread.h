b768afb7a172e63a9f71a919a736333deee4811f
Benoit Marcot <juz4m@hotmail.com>
2016-05-18 10:01:49 +0200

    mutex: Add pthread_mutex_trylock interface.

--- include/pthread.h
+++ include/pthread.h
@@ -19,7 +19,7 @@ int pthread_create(pthread_t *thread, /* const pthread_attr_t *attr, */
 #define PTHREAD_MUTEX_INITIALIZER (-1)
 
 int pthread_mutex_lock(pthread_mutex_t *mutex);
-//int pthread_mutex_trylock(pthread_mutex_t *mutex);
+int pthread_mutex_trylock(pthread_mutex_t *mutex);
 int pthread_mutex_unlock(pthread_mutex_t *mutex);
 
 #endif /* !PTHREAD_H */
