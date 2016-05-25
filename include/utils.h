9565748bc4b4c523a13988971ed9e00fac6c200e
Benoit Marcot <juz4m@hotmail.com>
2016-05-25 22:43:02 +0200

    utils: Use the WFI from CMSIS.

--- include/utils.h
+++ include/utils.h
@@ -9,22 +9,18 @@
 
 #ifndef __LINKER__
 
+#include "cmsis/arm/ARMCM4.h"
+
 static inline void infinite_loop(void)
 {
 	for (;;)
 		;
 }
 
-//XXX: Use the ARM CMSIS instead?
-static inline void __wfi(void)
-{
-	__asm__ __volatile__ ("wfi");
-}
-
 static inline void halt(void)
 {
 	for (;;)
-		__wfi();
+		__WFI();
 }
 
 char *strrev(char *s);
