2b5830618649d2327269aa772ad7a20299fb5a1a
Benoit Marcot <juz4m@hotmail.com>
2016-06-05 19:51:08 +0200

    Run pthread_create() in a coroutine.

--- sys/pthread.c
+++ sys/pthread.c
@@ -1,6 +1,10 @@
 #include "thread.h"
 #include "pthread.h"
 #include "sched-rr.h"
+#include "libc/ucontext.h"
+
+static ucontext_t main_context, pthread_context;
+static unsigned int ctx_stack[256];
 
 int pthread_yield_1(void)
 {
@@ -17,7 +21,7 @@ void pthread_exit_1(void *retval)
 	thread_exit(retval);
 }
 
-int pthread_create_1(/* __user */ pthread_t *thread, /* const pthread_attr_t *attr, */
+static int pthread_create_2(/* __user */ pthread_t *thread, /* const pthread_attr_t *attr, */
 		void *(*start_routine)(void *), void *arg)
 {
 	struct thread_info *thread_info;
@@ -31,4 +35,17 @@ int pthread_create_1(/* __user */ pthread_t *thread, /* const pthread_attr_t *at
 	sched_rr_add(thread_info);
 
 	return 0;
+}
+
+int pthread_create_1(/* __user */ pthread_t *thread, /* const pthread_attr_t *attr, */
+		void *(*start_routine)(void *), void *arg)
+{
+	/* link the current context to the print context */
+	pthread_context.uc_link = &main_context;
+	pthread_context.uc_stack.ss_sp = &ctx_stack[256];
+
+	/* pass the arguments to the new context, and swap */
+	makecontext(&pthread_context, pthread_create_2, 3, thread, start_routine,
+		arg);
+	swapcontext(&main_context, &pthread_context);
 }
