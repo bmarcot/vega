529ade7ea14a0e721b310face0657befc06a9890
Benoit Marcot <juz4m@hotmail.com>
2016-07-06 20:53:27 +0200

    (minor) Revisit <stdbool.h> header.

--- include/libc/stdbool.h
+++ include/libc/stdbool.h
@@ -1,9 +1,11 @@
-#ifndef LIBC_STDBOOL_H
-#define LIBC_STDBOOL_H
+#ifndef STDBOOL_H
+#define STDBOOL_H
 
-typedef _Bool bool;
+#define bool  _Bool
 
-#define false 0
-#define true  1
+#define true   1
+#define false  0
 
-#endif /* !LIBC_STDBOOL_H */
+#define __bool_true_false_are_defined  1
+
+#endif /* !STDBOOL_H */
