6aa5538821708a8411213612675aa7b13e9dc831
Benoit Marcot <juz4m@hotmail.com>
2016-07-06 13:14:50 +0200

    utils: Redefine MIN() using __auto_type.

--- include/utils.h
+++ include/utils.h
@@ -7,7 +7,19 @@
 #define align_hi(val, align)	\
 	(((val) + (align) - 1) & ~((align) - 1))
 
-#define MIN(a,b) ((a)<(b) ? (a):(b))
+#ifdef __GNUC__
+#  define MAX(a, b)		 \
+	({ __auto_type _a = (a); \
+	   __auto_type _b = (b); \
+	   _a > _b ? _a : _b; })
+#  define MIN(a, b)		 \
+	({ __auto_type _a = (a); \
+	   __auto_type _b = (b); \
+	   _a < _b ? _a : _b; })
+#else
+#  define MAX(a, b)  ((a) > (b) ? (a) : (b))
+#  define MIN(a, b)  ((a) < (b) ? (a) : (b))
+#endif
 
 #ifndef __LINKER__
 
