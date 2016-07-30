84ddfb520a1c428a0d2a1538c308092d3e570071
Benoit Marcot <juz4m@hotmail.com>
2016-07-30 18:35:35 +0200

    mutex: Rename header guards.

--- include/kernel/mutex.h
+++ include/kernel/mutex.h
@@ -1,9 +1,9 @@
-#ifndef MUTEX_H
-#define MUTEX_H
+#ifndef KERNEL_MUTEX_H
+#define KERNEL_MUTEX_H
 
 #include "linux/types.h"
 
 int mutex_lock(atomic_t /* __user  */ *lock);
 int mutex_unlock(atomic_t /* __user */ *lock);
 
-#endif /* !MUTEX_H */
+#endif /* !KERNEL_MUTEX_H */
