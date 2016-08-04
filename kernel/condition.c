fdd22d64b59d9094da4c4585081d714ffc5dcf46
Benoit Marcot <juz4m@hotmail.com>
2016-08-04 08:10:52 +0200

    cond-var: Cleanup file, add comments

--- kernel/condition.c
+++ kernel/condition.c
@@ -1,4 +1,4 @@
-/* mutex.c
+/* kernel/condition.c
  *
  * Copyright (c) 2016 Benoit Marcot
  */
@@ -11,34 +11,19 @@
 
 #include "linux/list.h"
 
-// user provides the storage (and kernel must do checks)
-
+/* The cond variable storage is provided by the user, and the kernel must check
+ * the addresses provided in the struct. See issue #8 (LDRT/STRT).  */
 int __pthread_cond_wait(pthread_cond_t *cond, pthread_mutex_t *mutex)
 {
-	// mutex is locked
-
-	// unlock mutex and add current thread to a list a thread blocking
-	// on the cond. a list ?
-
-
-	// create condition list
 	CURRENT_THREAD_INFO(cur_thread);
-	//list_add(&cur->q, &cond->list);
 	list_add_tail(&cur_thread->ti_q, &cond->waitq);
-
-	//IDEA: set priority as min, such that pthread_unlock puts so;ething else
-	// on the CPU?
-	// or change state ??
 	cur_thread->ti_state = THREAD_STATE_BLOCKED;
-
-	// 1. unlock mutex
-
 	__pthread_mutex_unlock(mutex);
 
 	//XXX: yield IS NOT mandatory; previous unlock may have rescheduled!
 	//__pthread_yield();
 
-	// 2. contend for the lock
+	/* contend for the lock */
 	__pthread_mutex_lock(mutex);
 
 	return 0;
@@ -50,15 +35,10 @@ int __pthread_cond_signal(pthread_cond_t *cond)
 	CURRENT_THREAD_INFO(cur_thread);
 
 	waiter = list_first_entry_or_null(&cond->waitq, struct thread_info, ti_q);
-
-	// nothing to wakeup
 	if (waiter == NULL)
 		return 0;
-
 	list_del(&waiter->ti_q);
 	sched_enqueue(waiter);
-
-	// si prio plus forte...
 	if (waiter->ti_priority >= cur_thread->ti_priority) {
 		sched_enqueue(cur_thread);
 		sched_elect(SCHED_OPT_NONE);
@@ -66,7 +46,3 @@ int __pthread_cond_signal(pthread_cond_t *cond)
 
 	return 0;
 }
-
-/* int pthread_cond_destroy(pthread_cond_t *cond); */
-
-/* pthread_cond_t cond = PTHREAD_COND_INITIALIZER; */
