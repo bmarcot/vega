0bc5dcf45c8cbd8bf196a4fbc031c85296e480e3
Benoit Marcot <juz4m@hotmail.com>
2016-09-02 11:26:10 +0200

    Use libc's offsetof macro in Linux headers

--- include/linux/stddef.h
+++ include/linux/stddef.h
@@ -6,15 +6,7 @@
 #ifndef _LINUX_STDDEF_H
 #define _LINUX_STDDEF_H
 
-#undef NULL
-#define NULL ((void *)0)
-
-#undef offsetof
-#ifdef __builtin_offsetof
-#define offsetof(TYPE, MEMBER)	__builtin_offsetof(TYPE, MEMBER)
-#else
-#define offsetof(TYPE, MEMBER)	((size_t)&((TYPE *)0)->MEMBER)
-#endif
+#include <stddef.h>
 
 /**
  * offsetofend(TYPE, MEMBER)
