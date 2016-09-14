c239570b3fd298bcac47d367a5cc5eda22dacf9c
Benoit Marcot <juz4m@hotmail.com>
2016-09-14 10:43:59 +0200

    libc: Define struct sigevent type

--- include/libc/signal.h
+++ include/libc/signal.h
@@ -1,12 +1,36 @@
 #ifndef SIGNAL_H
 #define SIGNAL_H
 
-#include <stddef.h>
+#include <sys/types.h>
 
 typedef struct {
 	void *ss_sp;     /* Base address of stack */
 	int ss_flags;    /* Flags */
 	size_t ss_size;	 /* Number of bytes in stack */
 } stack_t;
+
+/* sigevent - structure for notification from asynchronous routines */
+
+union sigval {            /* Data passed with notification */
+	int sival_int;    /* Integer value */
+	void *sival_ptr;  /* Pointer value */
+};
+
+struct sigevent {
+	int sigev_notify; /* Notification method */
+	int sigev_signo;  /* Notification signal */
+
+	/* Data passed with notification */
+	union sigval sigev_value;
+
+	/* Function used for thread notification (SIGEV_THREAD) */
+	void (*sigev_notify_function)(union sigval);
+
+	/* Attributes for notification thread (SIGEV_THREAD) */
+	void *sigev_notify_attributes;
+
+	/* ID of thread to signal (SIGEV_THREAD_ID) */
+	pid_t sigev_notify_thread_id;
+};
 
 #endif /* !SIGNAL_H */
