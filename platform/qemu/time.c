ed1ba456136aea5ef6a2017723849776f64eb4c2
Benoit Marcot <juz4m@hotmail.com>
2016-10-18 22:29:24 +0200

    (minor) Cleanup, remove commented code

--- platform/qemu/time.c
+++ platform/qemu/time.c
@@ -12,32 +12,10 @@
 #include "kernel.h"
 #include "systick.h"
 
-static LIST_HEAD(timers);
-//static LIST_HEAD(active_timers);
+static LIST_HEAD(timers);  /* active_timers */
 static LIST_HEAD(inactive_timers);
 
 extern u32 clocktime_in_msecs;
-
-/* int sys_timer_create(unsigned int msec) */
-/* { */
-/* 	/\* XXX: 1 timer per thread, replace wih malloc/free when implementing */
-/* 	        timer_create/timer_arm/signals timer management. *\/ */
-/* 	/\* struct timer timerp; *\/ */
-/* 	/\* CURRENT_THREAD_INFO(threadp); *\/ */
-
-/* 	/\* printk("timer: create a timer t=%dms at %p\n", msec, &timerp); *\/ */
-
-/* 	/\* timerp.owner = threadp; *\/ */
-/* 	/\* timerp.expire_clocktime = get_clocktime_in_msec() + msec; *\/ */
-/* 	/\* timerp.timer_type = TT_SLEEP; *\/ */
-/* 	/\* list_add(&timerp.list, &timers); *\/ */
-/* 	/\* sched_dequeue(threadp); *\/ */
-/* 	/\* sched_elect(SCHED_OPT_NONE); *\/ */
-
-/* 	//XXX: malloc timer? */
-
-/* 	return 0; */
-/* } */
 
 int __msleep(unsigned int msec)
 {
