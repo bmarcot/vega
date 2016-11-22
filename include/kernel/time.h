17f2ef922f3f508fbdb419d6838c93a8e46af015
Benoit Marcot <juz4m@hotmail.com>
2016-11-22 16:33:45 +0100

    time: Cleanup dead code

--- include/kernel/time.h
+++ include/kernel/time.h
@@ -10,25 +10,6 @@
 #include <sys/types.h>
 #include "linux/list.h"
 
-struct thread_info;
-
-enum timer_type {
-	TT_SLEEP,
-	TT_TIMER
-};
-
-struct timer_common {
-	timer_t            timerid;  //FIXME: harvest storage, timer_id + timer_type
-	u32                expire_clocktime;
-	enum timer_type    timer_type;
-	union {
-		struct thread_info *owner;  /* for sleep */
-		struct sigevent *sigev;     /* for timer */
-	};
-};
-
-/* common timer interface */
-
 struct timer_info;
 
 struct timer_operations {
