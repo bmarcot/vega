8d518ea58ac0a0cda952c0990b135a85eaabee55
Benoit Marcot <juz4m@hotmail.com>
2016-12-02 09:52:40 +0100

    Define platform halt function, move semihosting
    Any platform can do semihosting with proper debugger. Make the
    feature globally accessible for v7m architecture.

---
+++ include/arch/semihosting.h
@@ -0,0 +1,12 @@
+/*
+ * include/arch/semihosting.h
+ *
+ * Copyright (c) 2016 Benoit Marcot
+ */
+
+#ifndef _ARCH_SEMIHOSTING_H
+#define _ARCH_SEMIHOSTING_H
+
+void v7m_semih_exit(int status);
+
+#endif /* !_ARCH_SEMIHOSTING_H */
