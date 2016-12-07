0bf4eadbf5a31010c4c3cf7ba51e2c4e46ad16af
Benoit Marcot <juz4m@hotmail.com>
2016-12-07 10:17:04 +0100

    Add compiler abstaction/defines to kernel

---
+++ include/kernel/compiler.h
@@ -0,0 +1,15 @@
+/*
+ * include/kernel/compiler.h
+ *
+ * Copyright (c) 2016 Benoit Marcot
+ */
+
+#ifndef _KERNEL_COMPILER_H
+#define _KERNEL_COMPILER_H
+
+/* GCC weak symbol declaration */
+#ifndef __weak
+#  define __weak __attribute__((weak))
+#endif
+
+#endif /* !_KERNEL_COMPILER_H */
