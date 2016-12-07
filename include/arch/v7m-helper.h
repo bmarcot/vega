37dd19ec1a59525db17a9e1a9a0a1fcc48f69bff
Benoit Marcot <juz4m@hotmail.com>
2016-12-07 08:48:32 +0100

    Moved arch header file to include/

--- include/arch/v7m-helper.h
+++ include/arch/v7m-helper.h
@@ -1,11 +1,11 @@
 /*
- * include/kernel/v7m-helper.h
+ * include/arch/v7m-helper.h
  *
  * Copyright (c) 2016 Benoit Marcot
  */
 
-#ifndef KERNEL_V7M_HELPER_H
-#define KERNEL_V7M_HELPER_H
+#ifndef _ARCH_V7M_HELPER_H
+#define _ARCH_V7M_HELPER_H
 
 #define V7M_EXC_RETURN_HANDLER_MAIN   0xfffffff1
 #define V7M_EXC_RETURN_THREAD_MAIN    0xfffffff9
@@ -21,4 +21,4 @@ static inline void *v7m_clear_thumb_bit(void *addr)
     return (void *)((unsigned long)addr & ~1ul);
 }
 
-#endif /* !KERNEL_V7M_HELPER_H */
+#endif /* !_ARCH_V7M_HELPER_H */
