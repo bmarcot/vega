67f7f08cd7d128023534d88d821e547bafd2cc85
Benoit Marcot <juz4m@hotmail.com>
2016-12-17 16:36:32 +0100

    sys/minish: Handle insertion in middle of line

--- system/minish.c
+++ system/minish.c
@@ -4,12 +4,12 @@
  * Copyright (c) 2016 Benoit Marcot
  */
 
+#include <stdio.h>
 #include <string.h>
 #include <sys/types.h>
 
+#include "kernel.h" //XXX: printk()
 #include "minish.h"
-
-#include "kernel.h"
 #include "platform.h"
 
 int open(const char *pathname, int flags);
@@ -17,7 +17,7 @@ ssize_t read(int fd, void *buf, size_t count);
 ssize_t write(int fd, const void *buf, size_t count);
 void ls(void);
 
-static const char ESC_SEQ_CURSOR_BACKWARD[] = "\033[1D";
+static const char ESC_SEQ_CURSOR_BACKWARD[] = "\033[D";
 static const char ESC_SEQ_ERASE_LINE[]      = "\033[K";
 
 static const char TERM_PROMPT[]        = "$ ";
@@ -47,8 +47,10 @@ static void exec_command(const char *buf, int fd)
 	}
 }
 
-static int cur_pos = 0;
+static int cur;
+static int cur_eol;
 static char buf_line[MINISH_LINE_MAX];
+
 
 static void process_input(int fd)
 {
@@ -58,27 +60,49 @@ static void process_input(int fd)
 	switch (c) {
 	case ASCII_CARRIAGE_RETURN:
 		write(fd, TERM_CRLF, sizeof(TERM_CRLF) - 1);
-		if (cur_pos) {
-			buf_line[cur_pos] = ASCII_NULL;
+		if (cur) {
 			exec_command(buf_line, fd);
-			cur_pos = 0; /* relative position to prompt's last char */
+			cur = 0; /* relative position to prompt's last char */
+			cur_eol = 0;
 		}
 		write(fd, TERM_PROMPT, sizeof(TERM_PROMPT) - 1);
 		break;
 	case ASCII_BACKSPACE:
 	case ASCII_DELETE: //XXX: Qemu sends DEL instead of BS
-		if (cur_pos) {
+		if (cur) {
 			write(fd, ESC_SEQ_CURSOR_BACKWARD,
 				sizeof(ESC_SEQ_CURSOR_BACKWARD) - 1);
 			write(fd, ESC_SEQ_ERASE_LINE,
 				sizeof(ESC_SEQ_ERASE_LINE) - 1);
-			buf_line[--cur_pos] = ASCII_NULL;
+			buf_line[--cur] = ASCII_NULL;
+			cur_eol--;
 		}
 		break;
-	case ' ' ... '~': /* printable chars */
-		buf_line[cur_pos++] = c;
-		write(fd, &c, 1);
+	case ' ' ... '~':
+		if (cur < cur_eol) {
+			for (int i = cur_eol; i >= cur; i--)
+				buf_line[i + 1] = buf_line[i];
+		}
+		buf_line[cur++] = c;
+		buf_line[++cur_eol] = '\0';
+		char ebuf[16];
+		write(fd, &buf_line[cur - 1], strlen(&buf_line[cur - 1]));
+		if (cur < cur_eol) {
+			sprintf(ebuf, "\033[%dD", cur_eol - cur);
+			write(fd, ebuf, strlen(ebuf));
+		}
 		break;
+	case ASCII_ESCAPE:
+		read(fd, &c, 1);
+		if (c != '[') // this is not an escape sequence
+			return;
+		read(fd, &c, 1);
+		if (c != 'D') {
+			printk("Unhandled escape sequence!\n");
+			return;
+		}
+		cur--;
+		write(fd, "\033[D", 3);
 	default:
 		;
 	}
