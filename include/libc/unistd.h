0c2dde240b8ad2d11a972a13c5878c8b739b0b7a
Benoit Marcot <juz4m@hotmail.com>
2016-12-23 20:15:33 +0100

    libc: Add I/O functions declarations

--- include/libc/unistd.h
+++ include/libc/unistd.h
@@ -1,6 +1,9 @@
 #ifndef UNISTD_H
 #define UNISTD_H
 
+#include <sys/stat.h>
+#include <sys/types.h>
+
 enum sc_varname {
 	PAGESIZE,
 	_SC_CLK_TCK
@@ -11,5 +14,13 @@ long sysconf_1(int name);
 
 unsigned int msleep(unsigned int msecs);
 unsigned sleep(unsigned seconds);
+
+int open(const char *pathname, int flags);
+ssize_t read(int fd, void *buf, size_t count);
+ssize_t write(int fd, void *buf, size_t count);
+off_t seek(int fd, off_t offset, int whence);
+int stat(const char *pathname, struct stat *buf);
+int mount(const char *source, const char *target, const char *filesystemtype,
+	unsigned long mountflags, const void *data);
 
 #endif /* !UNISTD_H */
