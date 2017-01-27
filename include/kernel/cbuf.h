e85e4c9788277f8f80c0937d8d5d8a6ff1f93fb8
Benoit Marcot <juz4m@hotmail.com>
2017-01-27 11:19:07 +0100

    Add a lib for simple circular buffer

---
+++ include/kernel/cbuf.h
@@ -0,0 +1,23 @@
+/*
+ * include/kernel/cbuf.h
+ *
+ * Copyright (c) 2017 Benoit Marcot
+ */
+
+#ifndef _KERNEL_CBUF_H
+#define _KERNEL_CBUF_H
+
+#include <sys/types.h>
+
+struct cbuf_info {
+	size_t len;
+	int    pos_begin;
+	int    pos_end;
+	void   *buf;
+};
+
+void cbuf_init(struct cbuf_info *cbuf, void *buf, size_t len);
+int cbuf_getc(struct cbuf_info *cbuf, char *c);
+int cbuf_putc(struct cbuf_info *cbuf, char c);
+
+#endif /* !_KERNEL_CBUF_H */
