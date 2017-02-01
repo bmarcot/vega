3f3b68b4b257966f2ca76a29f1877c5877a45210
Benoit Marcot <juz4m@hotmail.com>
2017-02-01 20:54:49 +0100

    irq: Debug printf when attaching an isr

--- kernel/irq.c
+++ kernel/irq.c
@@ -1,8 +1,11 @@
 /*
  * kernel/irq.c
  *
- * Copyright (c) 2016 Benoit Marcot
+ * Copyright (c) 2016-2017 Benoit Marcot
  */
+
+#include <kernel/kernel.h>
+#include <kernel/types.h>
 
 #define IRQ_MAX  64
 
@@ -10,6 +13,9 @@ void (*irq_handler[IRQ_MAX])(void);
 
 void irq_attach(unsigned int irq, void (*handler)(void))
 {
+#ifdef DEBUG
+	printk("Attach IRQ % 2d to <%p>\n", irq, (u32)handler & ~1);
+#endif
 	if (irq < IRQ_MAX)
 		irq_handler[irq] = handler;
 }
