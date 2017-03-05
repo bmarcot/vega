3a4a33fe576a8331e812cb7a72cf9e3dbecd0482
Benoit Marcot <juz4m@hotmail.com>
2017-03-05 19:18:52 +0100

    libc/syscall: Support 6-args syscalls

--- libc/vega/syscalls.h
+++ libc/vega/syscalls.h
@@ -7,7 +7,11 @@ int do_syscall0(int no);
 int do_syscall1(void *a0, int no);
 int do_syscall2(void *a0, void *a1, int no);
 int do_syscall3(void *a0, void *a1, void *a2, int no);
-int do_syscall4(void *a0, void *a1, void *a2, void *a3, int no);
-int do_syscall5(void *a0, void *a1, void *a2, void *a3, void *a4, int no);
+int do_syscall4(void *a0, void *a1, void *a2, void *a3,
+		int no);
+int do_syscall5(void *a0, void *a1, void *a2, void *a3,
+		void *a4, int no);
+int do_syscall6(void *a0, void *a1, void *a2, void *a3,
+		void *a4, void *a5, int no);
 
 #endif /* !LIBC_SYSCALLS_H */
