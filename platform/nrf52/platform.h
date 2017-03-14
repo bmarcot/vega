dd0eaca511b330aa1cd79bc9e6a7013ea07db027
Benoit Marcot <juz4m@hotmail.com>
2017-03-14 09:12:06 +0100

    nrf52: Declare SPIM/TWIM config structs

--- platform/nrf52/platform.h
+++ platform/nrf52/platform.h
@@ -11,6 +11,21 @@
 #include <cmsis/nrf52/nrf52_bitfields.h>
 #include <cmsis/nrf52/nrf52832_peripherals.h>
 
+struct spim_config {
+	unsigned int pin_sck;
+	unsigned int pin_mosi;
+	unsigned int pin_miso;
+	unsigned int pin_cs;
+	unsigned int freq;
+};
+
+struct twim_config {
+	unsigned int pin_scl;
+	unsigned int pin_sda;
+	unsigned int freq;
+	unsigned int address;
+};
+
 void __platform_init(void);
 void __platform_halt(void);
 
