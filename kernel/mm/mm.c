ee50f5f9aed21df8bd6b9a6dca3af35dc5f6c9f4
Benoit Marcot <juz4m@hotmail.com>
2017-04-04 09:22:35 +0200

    mm: Unmap memory mapping, fix access

--- kernel/mm/mm.c
+++ kernel/mm/mm.c
@@ -7,26 +7,30 @@
 #include <errno.h>
 #include <string.h>
 #include <sys/cdefs.h>
+#include <sys/stat.h>
 #include <sys/types.h>
 
 #include <vega/sys/mman.h>
 
+#include <kernel/fs.h>
 #include <kernel/mm/page.h>
 
-#define M_ISANON(f)     (((f) & MAP_ANONYMOUS) == MAP_ANONYMOUS)
+#define M_ISANON(f)     (((f) & MAP_ANONYMOUS)     == MAP_ANONYMOUS)
 #define M_ISUNINIT(f)   (((f) & MAP_UNINITIALIZED) == MAP_UNINITIALIZED)
 
-void *sys_mmap(void *addr, size_t length, __unused int prot,
-	int flags, int fd, off_t offset)
+void *sys_mmap(void *addr, size_t length, int prot, int flags, int fd,
+	off_t offset)
 {
-	int order;
-
 	if (!length) {
 		errno = EINVAL;
 		return MAP_FAILED;
 	}
+	if (prot & PROT_NONE) {
+		errno = EACCES;
+		return MAP_FAILED;
+	}
 	if (M_ISANON(flags)) {
-		order = size_to_page_order(length);
+		int order = size_to_page_order(length);
 		addr = alloc_pages(order);
 		if (addr == NULL) {
 			errno = ENOMEM;
@@ -34,11 +38,14 @@ void *sys_mmap(void *addr, size_t length, __unused int prot,
 		}
 		if (!M_ISUNINIT(flags))
 			memset(addr, 0, length);
-
 	} else {
 		struct file *file = fd_to_file(fd);
 		if (!file) {
 			errno = EBADF;
+			return MAP_FAILED;
+		}
+		if (!S_ISREG(file->f_dentry->d_inode->i_mode)) {
+			errno = EACCES;
 			return MAP_FAILED;
 		}
 		if (vfs_mmap(file, offset, &addr))
@@ -46,4 +53,11 @@ void *sys_mmap(void *addr, size_t length, __unused int prot,
 	}
 
 	return addr;
+}
+
+int munmap(__unused void *addr, __unused size_t length)
+{
+	/* Closing the file descriptor does not unmap the region. */
+
+	return 0;
 }
