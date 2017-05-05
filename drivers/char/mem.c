475eeace84f6f59c947b541a779b1f76bddbc4be
Benoit Marcot <juz4m@hotmail.com>
2017-05-05 14:23:44 +0200

    char/mem: Use the revisited device model

--- drivers/char/mem.c
+++ drivers/char/mem.c
@@ -6,10 +6,14 @@
 
 #include <string.h>
 
+#include <kernel/device.h>
+#include <kernel/errno-base.h>
 #include <kernel/fs.h>
 #include <kernel/kernel.h>
 
-int mem_open(struct inode *inode, struct file *file)
+#define MEM_MAJOR  1
+
+int open_mem(struct inode *inode, struct file *file)
 {
 	(void)inode, (void)file;
 
@@ -60,58 +64,72 @@ ssize_t read_zero(struct file *file, char *buf, size_t count, off_t offset)
 }
 
 static const struct file_operations mem_fops = {
-	.open = mem_open,
+	.open = open_mem,
 	.read = read_mem,
 	.write = write_mem,
 };
 
 static const struct file_operations null_fops = {
-	.open = mem_open,
 	.read = read_null,
 	.write = write_null,
 };
 
 static const struct file_operations zero_fops = {
-	.open = mem_open,
 	.read = read_zero,
 	.write = write_null,
 };
 
 extern const struct file_operations random_fops;
-extern const struct inode_operations tmpfs_iops;
 
-static struct inode memdev_inodes[] = {
-	{	/* /dev/mem */
-		.i_ino  = 101,
-		.i_op   = &tmpfs_iops,
-		.i_fop  = &mem_fops,
-	},
-	{	/* /dev/null */
-		.i_ino  = 102,
-		.i_op   = &tmpfs_iops,
-		.i_fop  = &null_fops,
-	},
-	{	/* /dev/zero */
-		.i_ino  = 103,
-		.i_op   = &tmpfs_iops,
-		.i_fop  = &zero_fops,
-	},
-	{	/* /dev/random */
-		.i_ino  = 104,
-		.i_op   = &tmpfs_iops,
-		.i_fop  = &random_fops,
-	},
+/* "mem" devices:
+ *
+ *     PATH      MAJOR  MINOR
+ *     /dev/mem      1,     1
+ *     /dev/null     1,     2
+ *     /dev/zero     1,     3
+ *     /dev/random   1,     4  (implements a non-blocking PRNG)
+ */
+static const struct memdev {
+	const char                   *name;
+	umode_t                      mode;
+	const struct file_operations *fops;
+	fmode_t                      fmode;
+} devlist[] = {
+	[1] = { "mem",    0,    &mem_fops,    0 },
+	[2] = { "null",   0666, &null_fops,   0 },
+	[3] = { "zero",   0666, &zero_fops,   0 },
+	[4] = { "random", 0666, &random_fops, 0 },
+};
+
+static int memory_open(struct inode *inode, struct file *file)
+{
+	int minor;
+	const struct memdev *dev;
+
+	minor = MINOR(inode->i_rdev);
+	if (minor >= ARRAY_SIZE(devlist))
+		return -ENXIO;
+
+	dev = &devlist[minor];
+	if (!dev->fops)
+		return -ENXIO;
+
+	file->f_op = dev->fops;
+	file->f_mode |= dev->fmode;
+
+	if (dev->fops->open)
+		return dev->fops->open(inode, file);
+
+	return 0;
+}
+
+static const struct file_operations memory_fops = {
+        .open = memory_open,
 };
 
 void memdev_init(void)
 {
-	struct dentry dentry;
-	const char *names[] = { "mem", "null", "zero", "random" };
-
-	for (int i = 0; i < 4; i++) {
-		printk("Creating /dev/%s\n", names[i]);
-		dentry.d_inode = &memdev_inodes[i],
-		strcpy(dentry.d_name, names[i]);
-		vfs_link(0, dev_inode(), &dentry);
-	}
+	chrdev_register(MEM_MAJOR, &memory_fops);
+	for (int i = 1; i < ARRAY_SIZE(devlist); i++)
+		chrdev_add(MKDEV(MEM_MAJOR, i), devlist[i].name);
 }
