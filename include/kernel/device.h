31e8e0589f04b22eee3b0dd048e44d501a486432
Benoit Marcot <juz4m@hotmail.com>
2017-05-05 14:04:44 +0200

    device: Revisit the device model

---
+++ include/kernel/device.h
@@ -0,0 +1,23 @@
+/*
+ * include/kernel/device.h
+ *
+ * Copyright (c) 2017 Baruch Marcot
+ */
+
+#ifndef _KERNEL_DEVICE_H
+#define _KERNEL_DEVICE_H
+
+#include <kernel/fs.h>
+#include <kernel/types.h>
+
+#define MINORBITS      20
+#define MINORMASK      ((1U << MINORBITS) - 1)
+
+#define MAJOR(dev)     ((unsigned int) ((dev) >> MINORBITS))
+#define MINOR(dev)     ((unsigned int) ((dev) & MINORMASK))
+#define MKDEV(ma, mi)  (((ma) << MINORBITS) | (mi))
+
+int chrdev_register(unsigned int major, const struct file_operations *fops);
+int chrdev_add(kdev_t dev, const char *name);
+
+#endif /* !_KERNEL_DEVICE_H */
---
+++ kernel/device.c
@@ -0,0 +1,82 @@
+/*
+ * kernel/device.c
+ *
+ * Copyright (c) 2017 Baruch Marcot
+ */
+
+#include <string.h>
+#include <sys/stat.h>
+
+#include <kernel/device.h>
+#include <kernel/errno-base.h>
+#include <kernel/fs.h>
+#include <kernel/kernel.h>
+
+static struct char_dev {
+	const struct file_operations *fops;
+	unsigned int count;
+} char_devs[8];
+
+static inline struct char_dev *chrdev_lookup(unsigned int major)
+{
+	return &char_devs[major];
+}
+
+static int chrdev_open(struct inode *inode, struct file *file)
+{
+	struct char_dev *chrdev;
+
+	chrdev = chrdev_lookup(MAJOR(inode->i_rdev));
+	if (chrdev == NULL)
+		return -ENXIO;
+	file->f_op = chrdev->fops;
+	if (file->f_op->open)
+		return file->f_op->open(inode, file);
+
+	return 0;
+}
+
+static const struct file_operations def_chr_fops = {
+	.open = chrdev_open,
+};
+
+int chrdev_register(unsigned int major, const struct file_operations *fops)
+{
+	struct char_dev *chrdev;
+
+	chrdev = chrdev_lookup(major);
+	if (chrdev == NULL) {
+		pr_err("Cannot register device with major=%d", major);
+		return -ENXIO;
+	}
+	chrdev->fops = fops;
+	chrdev->count = 0;
+
+	return 0;
+}
+
+void init_special_inode(struct inode *inode, umode_t mode, kdev_t rdev)
+{
+	inode->i_mode = mode;
+	if (S_ISCHR(mode)) {
+		inode->i_fop = &def_chr_fops;
+		inode->i_rdev = rdev;
+	}
+}
+
+int chrdev_add(kdev_t dev, const char *name)
+{
+	struct dentry dentry;
+	struct inode *inode;
+
+	strcpy(dentry.d_name, name);
+	inode = __tmpfs_create(dev_inode(), &dentry, 0);
+	if (inode == NULL) {
+		pr_err("Cannot add device %s (%d, %d)", name, MAJOR(dev),
+			MINOR(dev));
+		return -1;
+	}
+	init_special_inode(inode, 0666 | S_IFCHR, dev);
+
+	return 0;
+}
