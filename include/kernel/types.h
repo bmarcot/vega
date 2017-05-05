ab00dbe7fb5d91abc11c476fff57c1dd6d0112c1
Benoit Marcot <juz4m@hotmail.com>
2017-05-05 13:37:49 +0200

    kernel: Declare fmode_t and kdev_t types

--- include/kernel/types.h
+++ include/kernel/types.h
@@ -49,6 +49,9 @@ typedef s16 __s16;
 typedef  u8 __u8;
 typedef  s8 __s8;
 
+typedef __u32          kdev_t;
+typedef unsigned short fmode_t;
+
 /* import struct list_head */
 #include "linux/types.h"
 
