e28fa95ce8849bb0231a53c99f80c6be1d4dd6fb
Benoit Marcot <juz4m@hotmail.com>
2017-05-23 15:02:32 +0200

    lib/array: Introduce macro to iterate an array
    Iterate over an array where the size of each element can be
    bigger than the size of its type.

---
+++ lib/array.h
@@ -0,0 +1,17 @@
+/*
+ * lib/array.h
+ *
+ * Copyright (c) 2017 Baruch Marcot
+ */
+
+#ifndef _LIB_ARRAY_H
+#define _LIB_ARRAY_H
+
+/* Iterate over an array of elements where each element can use
+ * more storage than the size of its type. */
+#define array_for_each_element(pos, first, num, size)		\
+	for (pos = (__typeof__(pos))(first);			\
+	     (char *)pos < (char *)(first) + (num) * (size);	\
+	     pos = (__typeof__(pos))((char *)pos + (size)))
+
+#endif /* !_LIB_ARRAY_H */
