4ddc18db53d3bd13bdb9cabc2a85aa52762ceaa7
Benoit Marcot <juz4m@hotmail.com>
2017-06-07 15:50:38 +0200

    Revert "libc: Delete <pthread>, replaced with Newlib"
    This reverts commit 461a4ee86efa903dcacbc4a515d9631d7e86604e.
    
    Our lightweight pthread library needs to define specific fields
    in pthread_attr_struct_t. We cannot reuse Newlib's pthread library
    which is tailored for Linux and bigger systems. Fix is to
    reintroduce our pthread function's forward declarations, and
    pthread types.

---
+++ include/libc/pthread.h
@@ -0,0 +1,37 @@
+#ifndef PTHREAD_H
+#define PTHREAD_H
+
+#include <sys/types.h>
+
+/* pthread attributes */
+
+int pthread_attr_init(pthread_attr_t *attr);
+int pthread_attr_setstacksize(pthread_attr_t *attr, size_t stacksize);
+int pthread_attr_getstacksize(const pthread_attr_t *attr, size_t *stacksize);
+
+/* multithreading */
+
+int pthread_yield(void);
+pthread_t pthread_self(void);
+void pthread_exit(void *);
+int pthread_create(pthread_t *thread, const pthread_attr_t *attr,
+		void *(*start_routine)(void *), void *arg);
+int pthread_join(pthread_t thread, void **retval);
+int pthread_detach(pthread_t thread);
+
+/* mutex */
+
+#define PTHREAD_MUTEX_INITIALIZER { .val = -1, }
+
+int pthread_mutex_init(pthread_mutex_t *mutex, const pthread_mutexattr_t *attr);
+int pthread_mutex_lock(pthread_mutex_t *mutex);
+int pthread_mutex_trylock(pthread_mutex_t *mutex);
+int pthread_mutex_unlock(pthread_mutex_t *mutex);
+
+/* cond variable */
+
+int pthread_cond_init(pthread_cond_t *cond, const pthread_condattr_t *attr);
+int pthread_cond_wait(pthread_cond_t *cond, pthread_mutex_t *mutex);
+int pthread_cond_signal(pthread_cond_t *cond);
+
+#endif /* !PTHREAD_H */
