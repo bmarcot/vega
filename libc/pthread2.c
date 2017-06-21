d616b696cf1873e28616c402de2e99378fd23e61
Benoit Marcot <juz4m@hotmail.com>
2017-06-21 22:14:07 +0200

    pthread2: Refactor pthread_join() and some others
      - pthread_attr_init()
      - pthread_detach()
      - pthread_exit()

--- libc/pthread2.c
+++ libc/pthread2.c
@@ -25,6 +25,9 @@ struct pthread {
 	void             *stackaddr;
 	unsigned long    stacksize;
 	int              flags;
+	_Atomic int      lock __attribute__((aligned(4)));
+	struct pthread   *joiner;
+	void             *retval;
 	struct list_head list;
 };
 
@@ -33,8 +36,14 @@ static LIST_HEAD(threads);
 int __pthread_trampoline(void *);
 
 #define PF_STACKALLOC 0x1
+#define PF_DETACHED   0x2
+#define PF_EXITING    0x4
 
-int pthread_create_2(pthread_t *thread, const pthread_attr_t *attr,
+#define IS_JOINED(pd)   ((pd)->joiner != NULL)
+#define IS_DETACHED(pd) ((pd)->flags & PF_DETACHED)
+#define IS_EXITING(pd)  ((pd)->flags & PF_EXITING)
+
+int pthread_create(pthread_t *thread, const pthread_attr_t *attr,
 		void *(*start_routine)(void *), void *arg)
 {
 	struct pthread *pthread;
@@ -47,7 +56,7 @@ int pthread_create_2(pthread_t *thread, const pthread_attr_t *attr,
 	else
 		stacksize = 512;
 
-	if (!attr || attr->stackaddr_set) {
+	if (!attr || !attr->stackaddr_set) {
 		stack = mmap(NULL, stacksize, 0, MAP_ANONYMOUS, 0, 0);
 		if (!stack)
 			return EAGAIN;
@@ -62,6 +71,8 @@ int pthread_create_2(pthread_t *thread, const pthread_attr_t *attr,
 	pthread->stackaddr = stack;
 	pthread->stacksize = stacksize;
 	pthread->flags = flags;
+	pthread->lock = 0;
+	pthread->joiner = NULL;
 	pr_info("pthread_struct at %p", pthread);
 
 	if (clone(__pthread_trampoline, pthread, 0, pthread) < 0) {
@@ -75,16 +86,45 @@ int pthread_create_2(pthread_t *thread, const pthread_attr_t *attr,
 	return 0;
 }
 
+static void release_pthread(struct pthread *pthread)
+{
+	pr_info("  releasae stack@%p", pthread->stackaddr);
+	if (pthread->flags & PF_STACKALLOC)
+		munmap((void *)pthread->stackaddr, pthread->stacksize);
+	list_del(&pthread->list);
+}
+
+// syscall wrapper
+
+#include <kernel/syscalls.h>
+#include "vega/syscalls.h"
+
+int SYS_futex(int *uaddr, int futex_op, int val)
+{
+	return do_syscall3((void *)uaddr, (void *)futex_op, (void *)val,
+			SYS_FUTEX);
+}
+
+// !syscall wrapper
+
+#include <uapi/kernel/futex.h>
+
 void SYS_exit(int status);
 
 void __pthread_exit(void *retval, struct pthread *pthread)
 {
-	pr_info("Freeing %d bytes of mem at %p (flags=%x)",
-		pthread->stacksize, pthread->stackaddr, pthread->flags);
+	pr_info("libpthread: exiting thread %p [stacksize=%p, flags=%x]",
+		pthread, pthread->stacksize, pthread->flags);
 
-	if (pthread->flags & PF_STACKALLOC)
-		munmap((void *)pthread->stackaddr, pthread->stacksize);
-	list_del(&pthread->list);
+	if (IS_DETACHED(pthread)) {
+		pr_info("exiting a detached thread");
+		release_pthread(pthread);
+	} else {
+		pthread->flags |= PF_EXITING;
+		pthread->retval = retval;
+		if (pthread->joiner)
+			SYS_futex((int *)&pthread->lock, FUTEX_WAKE, 1);
+	}
 	SYS_exit((int)retval);
 	/* if (list_is_singular(&threads)) */
 	/* 	do_syscall1((void *)retval, SYS_EXIT); */
@@ -101,7 +141,7 @@ __attribute__(( always_inline )) __STATIC_INLINE uint32_t __get_SP(void)
 	return result;
 }
 
-void pthread_exit_2(void *retval)
+struct pthread *__pthread_self(void)
 {
 	struct pthread *pthread;
 	unsigned long sp = __get_SP();
@@ -112,10 +152,47 @@ void pthread_exit_2(void *retval)
 	// RW register in Cortex-M that is not a Core register; but it does
 	// not exist so far...
 	list_for_each_entry(pthread, &threads, list) {
-		if ((pthread->stackaddr >= (void *)sp)
-			&& (pthread->stackaddr + pthread->stacksize <= (void *)sp))
+		if ((pthread->stackaddr <= (void *)sp)
+			&& (pthread->stackaddr + pthread->stacksize > (void *)sp))
 			break;
 	}
 
-	__pthread_exit(retval, pthread);
+	return pthread;
+}
+
+void pthread_exit(void *retval)
+{
+	__pthread_exit(retval, __pthread_self());
+}
+
+int pthread_join(pthread_t thread, void **retval)
+{
+	struct pthread *other = (struct pthread *)thread;
+
+	/* Is thread joinable? */
+	if (IS_DETACHED(other))
+		return -EINVAL;
+
+	/* Is somebody already waiting for that thread? */
+	if (IS_JOINED(other))
+		return -EINVAL;
+
+	if (!IS_EXITING(other)) {
+		other->joiner = __pthread_self();
+		//pr_info("other lock = %p", &other->lock);
+		SYS_futex((int *)&other->lock, FUTEX_WAIT, 0);
+	}
+	*retval = other->retval;
+	release_pthread(other);
+
+	return 0;
+}
+
+int pthread_detach(pthread_t thread)
+{
+	struct pthread *pthread = (struct pthread *)thread;
+
+	pthread->flags |= PF_DETACHED;
+
+	return 0;
 }
