c6e19bd76470defe03c48213b6ac1b7603c2fbb0
Benoit Marcot <juz4m@hotmail.com>
2017-07-17 11:22:22 +0200

    sched: Continue cleaning-up old thread subsystem

--- include/kernel/thread.h
+++ include/kernel/thread.h
@@ -8,75 +8,14 @@
 #define _KERNEL_THREAD_H
 
 #include <sys/pthread_types.h>
-#include <sys/types.h>
-
 #include <kernel/kernel.h>
-#include <kernel/types.h>
-
-#include "linux/list.h"
 
 #define INTR_STACK_ORDER  9  /* 512 Bytes */
 #define INTR_STACK_SIZE   (1 << INTR_STACK_ORDER)
 
-enum thread_privilege {
-	THREAD_PRIV_SUPERVISOR = 0,
-	THREAD_PRIV_USER       = 1
-};
-
-enum thread_state {
-	/* Thread structure allocated but not enqueued in the system scheduler. */
-	THREAD_STATE_NEW,
-
-	/* Ready to run in the system scheduler. */
-	THREAD_STATE_READY,
-
-	/* Running by the system scheduler. */
-	THREAD_STATE_RUNNING,
-
-	/* The thread has normally exited or has called Pthread_exit to exit. Its
-	 * resources have not been freed and will be freed if it is detached or
-	 * joined.    */
-	THREAD_STATE_TERMINATED,
-
-	/* Waiting for a mutex or resource. */
-	THREAD_STATE_BLOCKED
-};
-
-/*
- * This stackframe is built to handle the first scheduling of a task on
- * the CPU. Running a task for the first time is achieved in two stages.
- *
- * Stage 1:
- *   After switch_to() has switched the interrupt stacks for current and
- *   next tasks the function will restore the kernel-context for the next
- *   thread. That context is the non-scratch registers r4 to r11. At this
- *   point we don't care about the values in these registers: they are
- *   not used after that point during in that interrupt, and the next
- *   interrupt context will reinitilized them should it need to.
- *
- * Stage 2:
- *   After restoring the non-scratch registers we return from switch_to()
- *   and the value loaded in LR in stage1 is the address of task_kickstart().
- *   This function is a trampoline that emulate a return 'from interrupt
- *   sequence' by restoring the non-scratch registers for the tasks as
- *   well as triggering a switch from Handler_Mode to Thread_Mode in the
- *   CPU. We initialize the task non-scratch registers to 0.
- */
-
 /* forward declarations */
 
 struct thread_info;
-
-void thread_restore(struct thread_info *); //FIXME: rename to switch_to_restore_only ? meh..
-
-struct thread_info *thread_create(void *(*)(void *), void *,
-				enum thread_privilege, size_t);
-int thread_yield(void);
-int thread_self(void);
-void thread_exit(void *);
-int thread_set_priority(struct thread_info *thread, int priority);
-int thread_detach(pthread_t thread);
-struct thread_info *thread_clone(struct thread_info *other, void *arg);
 
 #define CURRENT_THREAD_INFO(var) \
 	struct thread_info *var = current_thread_info();
