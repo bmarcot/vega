e57a65b49ef2daf80bdd01696dda5d77cd2ac75c
Benoit Marcot <juz4m@hotmail.com>
2017-07-24 10:57:33 +0200

    libc/signal: Cleanup header, use kernel's uapi

--- include/vega/signal.h
+++ include/vega/signal.h
@@ -2,21 +2,13 @@
 #define LIBC_SIGNAL_H
 
 #include <sys/types.h>
-
-#include <kernel/types.h>
-
 #include <uapi/kernel/signal.h>
 
 typedef struct {
-	void *ss_sp;     /* Base address of stack */
-	int ss_flags;    /* Flags */
-	size_t ss_size;	 /* Number of bytes in stack */
+	void   *ss_sp;
+	int    ss_flags;
+	size_t ss_size;
 } stack_t;
-
-#define SIGKILL  9   /* Kill (can't be caught or ignored) (POSIX) */
-#define SIGUSR1  10  /* User defined signal 1 (POSIX) */
-#define SIGUSR2  12  /* User defined signal 2 (POSIX) */
-#define SIGSTOP  19  /* Stop executing(can't be caught or ignored) (POSIX) */
 
 int sigaction(int sig, const struct sigaction *restrict act,
 	struct sigaction *restrict oact);
