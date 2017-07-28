34ab08260ef61bf59422fdc1f2352f1b058f7739
Benoit Marcot <juz4m@hotmail.com>
2017-07-28 14:18:32 +0200

    signal: Revisit signal sending to current task

--- kernel/signal.c
+++ kernel/signal.c
@@ -13,27 +13,10 @@
 #include <kernel/list.h>
 #include <kernel/sched.h>
 #include <kernel/signal.h>
-#include <kernel/thread.h>
 
 #include <asm/current.h>
 #include <asm/thread_info.h>
 #include <asm/v7m-helper.h>
-
-#include <uapi/kernel/signal.h>
-
-void *v7m_alloca_thread_context(struct thread_info *tip, size_t len)
-{
-	tip->thread_ctx.sp -= len;
-
-	return (void *)tip->thread_ctx.sp;
-}
-
-void v7m_push_thread_context(struct thread_info *tip, void *data, size_t len)
-{
-	void *stack_pointer = v7m_alloca_thread_context(tip, len);
-
-	memcpy(stack_pointer, data, len);
-}
 
 //FIXME: this is part of the task_struct->signal_struct
 static LIST_HEAD(ksignals); /* list of installed handlers */
@@ -86,11 +69,28 @@ int sys_sigaction(int signum, const struct sigaction *act,
 	return 0;
 }
 
-static inline void init_sigctx(struct cpu_saved_context *sigctx, u32 arg1,
-			u32 arg2, struct sigaction *sa)
+#define __USER_STACK_ALLOCA(__ptr, __align) ({		 \
+	u32 __sp = current_thread_info()->thread_ctx.sp; \
+	__sp -= sizeof(__typeof__(*(__ptr)));		 \
+	__sp = align(__sp, __align);			 \
+	current_thread_info()->thread_ctx.sp = __sp;	 \
+	__ptr = (__typeof__(__ptr))__sp; })
+
+static void __send_signal(int sig, struct sigaction *sa, union sigval value)
 {
-	sigctx->r1 = arg1; /* siginfo_t or nil */
-	sigctx->r2 = arg2; /* ucontext_t *, but commonly unused */
+	siginfo_t *siginfo = NULL;
+	struct cpu_saved_context *sigctx;
+
+	if (sa->sa_flags & SA_SIGINFO) {
+		__USER_STACK_ALLOCA(siginfo, 4);
+		siginfo->si_signo = sig;
+		siginfo->si_value = value;
+		siginfo->si_pid = current->pid;
+	}
+
+	__USER_STACK_ALLOCA(sigctx, 8);
+	sigctx->r1 = (u32)siginfo; /* siginfo_t or nil */
+	sigctx->r2 = 0;            /* ucontext_t *, but commonly unused */
 	sigctx->r3 = 0;
 	sigctx->r12 = 0;
 	if (sa->sa_flags & SA_RESTORER)
@@ -101,55 +101,26 @@ static inline void init_sigctx(struct cpu_saved_context *sigctx, u32 arg1,
 	sigctx->xpsr = xPSR_T_Msk;
 }
 
-static void do_handler(struct sigaction *sa)
-{
-	v7m_alloca_thread_context(current_thread_info(),
-				sizeof(struct cpu_saved_context));
-	struct cpu_saved_context *sigctx =
-		current_thread_info()->thread_ctx.ctx;
-	init_sigctx(sigctx, 0, 0, sa);
-}
-
-static void do_sigaction(int sig, struct sigaction *sa, union sigval value)
-{
-	v7m_alloca_thread_context(current_thread_info(),
-				align_next(sizeof(siginfo_t), 8));
-	siginfo_t *siginfop =
-		(siginfo_t *)current_thread_info()->thread_ctx.sp;
-	siginfop->si_signo = sig;
-	siginfop->si_value = value;
-	siginfop->si_pid = current->pid;
-
-	v7m_alloca_thread_context(current_thread_info(),
-				sizeof(struct cpu_saved_context));
-	struct cpu_saved_context *sigctx =
-		current_thread_info()->thread_ctx.ctx;
-	init_sigctx(sigctx, (u32)siginfop, 0, sa);
-}
-
-static int do_sigqueue(__unused pid_t pid, int sig, union sigval value)
+static int send_signal(__unused pid_t pid, int sig, union sigval value)
 {
 	struct ksignal *ks = get_ksignal(sig);
 	if (!ks)
 		return -EINVAL;
 
 	current->sig = sig;
-	if (ks->sa.sa_flags & SA_SIGINFO)
-		do_sigaction(sig, &ks->sa, value);
-	else
-		do_handler(&ks->sa);
+	__send_signal(sig, &ks->sa, value);
 
 	return sig;
 }
 
 int sys_sigqueue(pid_t pid, int sig, union sigval value)
 {
-	return do_sigqueue(pid, sig, value);
+	return send_signal(pid, sig, value);
 }
 
 int sys_kill(pid_t pid, int sig)
 {
-	return do_sigqueue(pid, sig, (union sigval){0});
+	return send_signal(pid, sig, (union sigval){0});
 }
 
 int sys_sigreturn(void)
@@ -172,5 +143,5 @@ void do_sigevent(struct task_struct *tsk, struct sigevent *sigevent)
 
 	if (sigevent->sigev_notify != SIGEV_SIGNAL)
 		return;
-	do_sigqueue(0, sigevent->sigev_signo, sigevent->sigev_value);
+	send_signal(0, sigevent->sigev_signo, sigevent->sigev_value);
 }
