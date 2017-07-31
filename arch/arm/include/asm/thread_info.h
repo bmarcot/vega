2900b61f121616fac28cdec982a9f0dd99734cfe
Benoit Marcot <juz4m@hotmail.com>
2017-07-31 13:27:50 +0200

    arch/arm: Save non-scratch regs on interrupt entry

--- arch/arm/include/asm/thread_info.h
+++ arch/arm/include/asm/thread_info.h
@@ -10,7 +10,7 @@
 #include <kernel/log2.h>
 #include <kernel/types.h>
 
-struct preserved_context {
+struct cpu_kernel_context {
 	u32 r4;  /* zero-filled */
 	u32 r5;
 	u32 r6;
@@ -23,29 +23,41 @@ struct preserved_context {
 	u32 lr;  /* loaded with EXC_RETURN value */
 };
 
-struct cpu_saved_context {
-	u32 r0;        /* args or zero-filled */
+struct cpu_user_context {
+	/* preserved registers on interrupt entry */
+	u32 r4;
+	u32 r5;
+	u32 r6;
+	u32 r7;
+	u32 r8;
+	u32 r9;
+	u32 r10;
+	u32 r11;
+
+	/* cpu-saved registers */
+	u32 r0;
 	u32 r1;
 	u32 r2;
 	u32 r3;
 	u32 r12;
-	u32 lr;        /* initially loaded with pthread_exit() */
-	u32 ret_addr;  /* thread entry-point function */
-	u32 xpsr;      /* forced to Thumb_Mode */
+	u32 lr;
+	u32 ret_addr;
+	u32 xpsr;
 };
 
 struct task_struct;
 
 struct thread_info {
 	union {
-		__u32 sp; /* msp special register */
-		struct preserved_context *ctx;
-	} kernel_ctx;
+		u32 msp;
+		struct cpu_kernel_context *ctx;
+	} kernel;
 	union {
-		__u32 sp; /* psp special register */
-		struct cpu_saved_context *ctx;
-	} thread_ctx;
-	__u32 priv;
+		u32 psp;
+		struct cpu_user_context *ctx;
+	} user;
+	u32 priv;
+
 #ifndef CONFIG_THREAD_INFO_IN_TASK
 	struct task_struct *task;
 #endif
