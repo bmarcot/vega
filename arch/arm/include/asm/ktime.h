c90579287386b6793d77f87c55e2a93216fb2fe3
Benoit Marcot <juz4m@hotmail.com>
2017-08-22 18:12:21 +0200

    arch/arm: Define ktime_t as a signed 64-bit integer

---
+++ arch/arm/include/asm/ktime.h
@@ -0,0 +1,14 @@
+/*
+ * arch/arm/include/asm/ktime.h
+ *
+ * Copyright (c) 2017 Benoit Marcot
+ */
+
+#ifndef _ASM_KTIME_H
+#define _ASM_KTIME_H
+
+#include <kernel/types.h>
+
+typedef s64 ktime_t;
+
+#endif /* !_ASM_KTIME_H */
