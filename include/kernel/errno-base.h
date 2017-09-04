2f2f057144c7b9dbe85083249da291b1ebe94bdf
Benoit Marcot <juz4m@hotmail.com>
2017-09-04 14:05:10 +0200

    core: Define ENOSYS error number

--- include/kernel/errno-base.h
+++ include/kernel/errno-base.h
@@ -36,4 +36,12 @@
 #define	EDOM		33	/* Math argument out of domain of func */
 #define	ERANGE		34	/* Math result not representable */
 
+/*
+ * Extra codes
+ */
+
+#ifndef ENOSYS
+#define ENOSYS          78      /* Function not implemented */
+#endif
+
 #endif /* !_KERNEL_ERRNO_BASE_H_ */
