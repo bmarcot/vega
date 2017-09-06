0c16600e507cbd828ad5d7528cd9dbc709b6a392
Benoit Marcot <juz4m@hotmail.com>
2017-09-06 09:45:33 +0200

    drivers/clocksource: Support SysTick as clksrc

---
+++ drivers/clocksource/systick.c
@@ -0,0 +1,68 @@
+/*
+ * drivers/clocksource/systick.c
+ *
+ * Copyright (c) 2017 Benoit Marcot
+ */
+
+#include <kernel/time/clocksource.h>
+#include <kernel/types.h>
+
+#include <asm/ktime.h>
+
+#include "platform.h"
+
+static u64 ticks;
+
+#define SYSTICK_FREQ_IN_HZ 1000
+#define SYSTICK_PERIOD_IN_MSECS (SYSTICK_FREQ_IN_HZ / 1000)
+
+void systick(void)
+{
+	ticks++;
+}
+
+ktime_t systick_read(struct clocksource *cs)
+{
+	(void)cs;
+
+#define NSEC_PER_MSEC 1000000l
+	return ticks * (ktime_t)NSEC_PER_MSEC;
+}
+
+void systick_suspend(struct clocksource *cs)
+{
+	(void)cs;
+
+	SysTick_Config(0);
+}
+
+void systick_resume(struct clocksource *cs)
+{
+	(void)cs;
+
+	SysTick_Config(SystemFrequency / SYSTICK_FREQ_IN_HZ);
+}
+
+struct clocksource clocksource_systick = {
+	.read = systick_read,
+	.suspend = systick_suspend,
+	.resume = systick_resume,
+};
+
+/* int systick_clocksource_init(void) */
+/* { */
+/* /\* 	exynos4_mct_frc_start(); *\/ */
+
+/* /\* #if defined(CONFIG_ARM) *\/ */
+/* /\* 	exynos4_delay_timer.read_current_timer = &exynos4_read_current_timer; *\/ */
+/* /\* 	exynos4_delay_timer.freq = clk_rate; *\/ */
+/* /\* 	register_current_timer_delay(&exynos4_delay_timer); *\/ */
+/* /\* 	#endif *\/ */
+
+/* /\* 	if (clocksource_register_hz(&mct_frc, clk_rate)) *\/ */
+/* /\* 		panic("%s: can't register clocksource\n", mct_frc.name); *\/ */
+
+/* 	sched_clock_register(exynos4_read_sched_clock, 32, clk_rate); */
+
+/* 	return 0; */
+/* } */
