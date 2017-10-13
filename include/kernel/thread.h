9cf0c89cc9191e218ced628a6b23ee7f64102e51
Benoit Marcot <juz4m@hotmail.com>
2017-10-13 16:15:14 +0200

    thread: Delete dead code

--- include/kernel/thread.h
+++ include/kernel/thread.h
@@ -7,23 +7,6 @@
 #ifndef _KERNEL_THREAD_H
 #define _KERNEL_THREAD_H
 
-#include <sys/pthread_types.h>
-#include <kernel/kernel.h>
-
-#define INTR_STACK_ORDER  9  /* 512 Bytes */
-#define INTR_STACK_SIZE   (1 << INTR_STACK_ORDER)
-
-/* forward declarations */
-
-struct thread_info;
-
-#define CURRENT_THREAD_INFO(var) \
-	struct thread_info *var = current_thread_info();
-
-#define THREAD_INFO(addr)					\
-	({ (struct thread_info *)align((unsigned long)addr,	\
-					INTR_STACK_SIZE); })
-
 #define THREAD_GROUP_MAX	32
 #define THREAD_MAX		256
 
