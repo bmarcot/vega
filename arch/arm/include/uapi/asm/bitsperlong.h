970c1eb0642052f153b301adb768d4ab2051c00f
Benoit Marcot <juz4m@hotmail.com>
2017-11-28 15:39:46 +0100

    arm: Define number of bits in type long

---
+++ arch/arm/include/uapi/asm/bitsperlong.h
@@ -0,0 +1,13 @@
+/*
+ * arch/arm/include/uapi/asm/bitsperlong.h
+ *
+ * Copyright (c) 2017 Baruch Marcot
+ *
+ */
+
+#ifndef _ASM_BITSPERLONG_H
+#define _ASM_BITSPERLONG_H
+
+#define __BITS_PER_LONG	32
+
+#endif /* !_ASM_BITSPERLONG_H */
