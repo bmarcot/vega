69b80593b5f0b7472dd2e10c4a3cc5631ab78ef7
Benoit Marcot <juz4m@hotmail.com>
2017-11-28 08:35:08 +0100

    arm/signal: Refactor function prototype

--- arch/arm/kernel/signal.c
+++ arch/arm/kernel/signal.c
@@ -4,6 +4,8 @@
  * Copyright (c) 2017 Baruch Marcot
  */
 
+#include <string.h>
+
 #include <kernel/kernel.h>
 #include <kernel/sched.h>
 #include <kernel/signal.h>
@@ -23,24 +25,20 @@
 
 #define __process_alloca(__ptr) __process_alloca_with_align(__ptr, 1)
 
-static void setup_sigframe(int sig, struct sigaction *sa, int value)
+static void setup_sigframe(struct sigqueue *q, struct sigaction *sa)
 {
 	siginfo_t *siginfo = NULL;
 	struct cpu_user_context *sigctx;
 
 	if (sa->sa_flags & SA_SIGINFO) {
 		__process_alloca(siginfo);
-		siginfo->si_signo = sig;
-		siginfo->si_value.sival_int = value;
-		siginfo->si_pid = current->tgid;
+		memcpy(siginfo, &q->info, sizeof(*siginfo));
 	}
 
 	__process_alloca_with_align(sigctx, 8);
-	sigctx->r0 = sig;          /* signum */
-	sigctx->r1 = (u32)siginfo; /* siginfo_t or nil */
-	sigctx->r2 = 0;            /* ucontext_t *, but commonly unused */
-	sigctx->r3 = 0;
-	sigctx->r12 = 0;
+	sigctx->r0 = q->info.si_signo;  /* signum */
+	sigctx->r1 = (u32)siginfo;      /* siginfo_t or null */
+	sigctx->r2 = 0;                 /* ucontext_t *, commonly unused */
 	if (sa->sa_flags & SA_RESTORER)
 		sigctx->lr = (u32)v7m_set_thumb_bit(sa->sa_restorer);
 	else
@@ -56,7 +54,6 @@ void do_exit(int status);
 
 static int do_signal(void)
 {
-	struct sigaction *act;
 	struct sigqueue *sig = list_first_entry(&current->pending.list,
 						struct sigqueue, list);
 	int signo = sig->info.si_signo;
@@ -64,15 +61,14 @@ static int do_signal(void)
 	if (signo == SIGKILL)
 		do_exit(0);
 
-	act = &current->sighand->action[signo];
-	setup_sigframe(signo, act, sig->info.si_value.sival_int);
+	setup_sigframe(sig, &current->sighand->action[signo]);
 
 	return 0;
 }
 
 void do_notify_resume(int syscall_retval)
 {
-	/* save the syscall return value into the syscall frame */
+	/* save the return value that will be restored by sigreturn() */
 	current_thread_info()->user.ctx->r0 = syscall_retval;
 
 	if (test_thread_flag(TIF_SIGPENDING))
