a9d772e248065227e896d9285cdac0164d5c0fd5
Benoit Marcot <juz4m@hotmail.com>
2018-03-01 11:32:27 +0100

    sched: Define CLONE_SIGHAND flag

--- include/uapi/kernel/sched.h
+++ include/uapi/kernel/sched.h
@@ -1,14 +1,14 @@
 /*
  * include/uapi/kernel/sched.h
  *
- * Copyright (c) 2017 Baruch Marcot
- *
+ * Copyright (c) 2017-2018 Benoit Marcot
  */
 
 #ifndef _UAPI_KERNEL_SCHED_H
 #define _UAPI_KERNEL_SCHED_H
 
-#define CLONE_VFORK	0x00004000 /* child waked up parent on exit */
-#define CLONE_THREAD	0x00010000 /* same thread group? */
+#define CLONE_VFORK	0x00004000	/* child waked up parent on exit */
+#define CLONE_SIGHAND	0x00000800	/* set if signal handlers and blocked signals shared */
+#define CLONE_THREAD	0x00010000	/* same thread group? */
 
 #endif /* !_UAPI_KERNEL_SCHED_H */
