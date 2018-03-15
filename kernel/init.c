596e5ae272adc0f70956a37d436007795b886127
Benoit Marcot <juz4m@hotmail.com>
2018-03-15 14:42:37 +0100

    init: Initialize signal structs

--- kernel/init.c
+++ kernel/init.c
@@ -12,11 +12,12 @@
 #include <kernel/signal_types.h>
 #include <kernel/stddef.h>
 
-#define CONFIG_INIT_STACK_SIZE	512
+#define CONFIG_INIT_STACK_SIZE	512 //FIXME: get stack size from??
 
-int main();
+int main(); //FIXME: unit tests support
 
 static char stack[CONFIG_INIT_STACK_SIZE] __attribute__((aligned(8)));
+
 static struct signal_struct signal;
 static struct sighand_struct sighand;
 
@@ -42,9 +43,13 @@ struct task_struct *alloc_init_task(void)
 	init->parent = NULL;
 
 	/* signals */
+	for (int i = 0; i < _NSIG - 1; i++)
+		sighand.action[i].sa_handler = SIG_DFL;
+	INIT_LIST_HEAD(&signal.thread_head);
 	init->signal = &signal;
 	init->sighand = &sighand;
-	// conflicting with signal unit tests, because they run as init process
+	//FIXME: Conflicting with unit tests, because they run as init process.
+	// Unit tests should run as new process instead (started by exec()).
 	sigemptyset(&init->blocked); //sigfillset(&init->blocked);
 	init_sigpending(&init->pending);
 
