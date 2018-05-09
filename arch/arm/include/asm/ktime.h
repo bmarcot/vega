09fc9dc1adbc745cfbd10292d173126d0f99affb
Benoit Marcot <juz4m@hotmail.com>
2018-05-09 10:21:02 +0200

    arm: Define KTIME_MAX value (64-bit signed)

--- arch/arm/include/asm/ktime.h
+++ arch/arm/include/asm/ktime.h
@@ -1,13 +1,15 @@
 /*
  * arch/arm/include/asm/ktime.h
  *
- * Copyright (c) 2017 Benoit Marcot
+ * Copyright (c) 2017-2018 Benoit Marcot
  */
 
 #ifndef _ASM_KTIME_H
 #define _ASM_KTIME_H
 
 #include <kernel/types.h>
+
+#define KTIME_MAX ((s64)~(1ll << 63))
 
 typedef s64 ktime_t;
 
