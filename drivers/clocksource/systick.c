38f40a5aef8cb61393d496582e29927fcf4d0245
Benoit Marcot <juz4m@hotmail.com>
2018-05-16 14:50:47 +0200

    arm: Add control functions to SysTick clocksource

--- drivers/clocksource/systick.c
+++ drivers/clocksource/systick.c
@@ -39,23 +39,41 @@ read_val:
 	return cycle_count + 0xffffff - val;
 }
 
-static void systick_suspend(struct clocksource *cs)
+static inline int systick_enable(struct clocksource *cs)
+{
+	cycle_count = 0;
+
+	SysTick->LOAD = 0xffffff; /* Max value */
+	SysTick->VAL = 0;
+	SysTick->CTRL = SysTick_CTRL_CLKSOURCE_Msk | SysTick_CTRL_TICKINT_Msk
+		| SysTick_CTRL_ENABLE_Msk;
+
+	return 0;
+}
+
+static inline void systick_disable(struct clocksource *cs)
 {
 	SysTick->CTRL = 0;
+	SysTick->LOAD = 0;
+	SysTick->VAL = 0;
+}
+
+static void systick_suspend(struct clocksource *cs)
+{
+	SysTick->CTRL &= ~SysTick_CTRL_ENABLE_Msk;
 }
 
 static void systick_resume(struct clocksource *cs)
 {
-	SysTick->LOAD = 0xffffff;
-	SysTick->VAL = 0;
-	SysTick->CTRL = SysTick_CTRL_CLKSOURCE_Msk
-		| SysTick_CTRL_TICKINT_Msk
-		| SysTick_CTRL_ENABLE_Msk;
+	/* Writing to SYST_CSR in Qemu will clear SYST_CVR */
+	SysTick->CTRL |= SysTick_CTRL_ENABLE_Msk;
 }
 
 void clocksource_init_systick(struct clocksource *cs)
 {
 	cs->read = systick_read;
+	cs->enable = systick_enable;
+	cs->disable = systick_disable;
 	cs->suspend = systick_suspend;
 	cs->resume = systick_resume;
 }
