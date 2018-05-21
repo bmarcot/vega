e8a2244c349c3daf1c932e20d04a53a49ba4d9ba
Benoit Marcot <juz4m@hotmail.com>
2018-05-21 11:19:47 +0200

    core: Add standard headers stdarg.h, stdio.h

---
+++ include/kernel/stdarg.h
@@ -0,0 +1,12 @@
+/*
+ * include/kernel/stdarg.h
+ *
+ * Copyright (c) 2018 Benoit Marcot
+ */
+
+#ifndef _KERNEL_STDARG_H
+#define _KERNEL_STDARG_H
+
+#include <stdarg.h>
+
+#endif /* !_KERNEL_STDARG_H */
---
+++ include/kernel/stdio.h
@@ -0,0 +1,13 @@
+/*
+ * include/kernel/stdio.h
+ *
+ * Copyright (c) 2018 Benoit Marcot
+ */
+
+#ifndef _KERNEL_STDIO_H
+#define _KERNEL_STDIO_H
+
+int putchar(int c);
+int puts(const char *s);
+
+#endif /* !_KERNEL_STDIO_H */
