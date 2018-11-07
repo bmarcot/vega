d346d00662d6076503f3eaa1b612139c2ff7570e
Benoit Marcot <juz4m@hotmail.com>
2018-11-07 12:55:08 +0100

    futex: Replace old logic with a waitqueue

--- include/kernel/futex.h
+++ include/kernel/futex.h
@@ -1,7 +1,7 @@
 /*
  * include/kernel/futex.h
  *
- * Copyright (c) 2017 Benoit Marcot
+ * Copyright (c) 2017-2018 Benoit Marcot
  */
 
 #ifndef _KERNEL_FUTEX_H
@@ -12,9 +12,9 @@
 struct task_struct;
 
 struct futex {
-	int                *lock_ptr;
-	struct task_struct *task;
-	struct list_head   list;
+	int			*lock_ptr;
+	struct list_head	wq_head;
+	struct list_head	list;
 };
 
 int futex_wait(int *uaddr, int val);
