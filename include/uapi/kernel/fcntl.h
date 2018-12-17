cf3cbb992e7b98231f6cc98c6c90d7bdacc1b215
Benoit Marcot <juz4m@hotmail.com>
2018-12-17 15:12:24 +0100

    uapi: Declare a few mode opening flags

---
+++ include/uapi/kernel/fcntl.h
@@ -0,0 +1,16 @@
+/*
+ * include/uapi/kernel/fcntl.h
+ *
+ * Copyright (c) 2018 Benoit Marcot
+ */
+
+#ifndef _UAPI_KERNEL_FCNTL_H
+#define _UAPI_KERNEL_FCNTL_H
+
+#define O_APPEND	0x0008
+#define O_NONBLOCK	0x0080
+#define O_CREAT		0x0100
+#define O_TRUNC		0x0200
+#define O_EXCL		0x0400
+
+#endif /* !_UAPI_KERNEL_FCNTL_H */
--- libc/include/fcntl.h
+++ libc/include/fcntl.h
@@ -1,3 +1,5 @@
 #pragma once
 
+#include <uapi/kernel/fcntl.h>
+
 int open(const char *pathname, int flags);
