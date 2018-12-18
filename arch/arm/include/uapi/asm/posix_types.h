067676703826df8270ada28357458ab0b92ff7c9
Benoit Marcot <juz4m@hotmail.com>
2018-12-18 20:19:09 +0100

    arm: Define the posix type mode_t

--- arch/arm/include/uapi/asm/posix_types.h
+++ arch/arm/include/uapi/asm/posix_types.h
@@ -1,8 +1,7 @@
 /*
  * arch/arm/include/uapi/asm/posix_types.h
  *
- * Copyright (c) 2017 Baruch Marcot
- *
+ * Copyright (c) 2017-2018 Benoit Marcot
  */
 
 #ifndef _ASM_POSIX_TYPES_H
@@ -10,11 +9,12 @@
 
 typedef int           clock_t;
 typedef int           clockid_t;
-typedef unsigned int  dev_t;
-typedef unsigned int  ino_t;
-typedef int           off_t;
+typedef unsigned int	dev_t;	/* device number */
+typedef unsigned int	ino_t;	/* inode number */
+typedef int		off_t;	/* file offset */
 typedef int           id_t;
-typedef int           pid_t;
+typedef unsigned short	mode_t; /* permissions */
+typedef int		pid_t;	/* process id */
 typedef unsigned int  rlim_t;
 typedef unsigned int  size_t;
 typedef int           ssize_t;
