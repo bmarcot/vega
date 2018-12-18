7f24887a32ef2ead6e4db13f6ceb19680abaa540
Benoit Marcot <juz4m@hotmail.com>
2018-12-18 20:21:22 +0100

    include/kernel/mqueue.h - define struct mq_attr

--- include/kernel/mqueue.h
+++ include/kernel/mqueue.h
@@ -9,9 +9,17 @@
 
 #include <kernel/types.h>
 
+struct mq_attr {
+	long	mq_flags;	/* Flags: 0 or O_NONBLOCK */
+	long	mq_maxmsg;	/* Max. # of messages on queue */
+	long	mq_msgsize;	/* Max. message size (bytes) */
+	long	mq_curmsgs;	/* # of messages currently in queue */
+};
+
 struct mqdes {
-	char			name[16];
-	int			flags;
+	char			name[16]; //FIXME: This will be replaced by an inode in tmpfs
+	struct mq_attr		attr;
+	int			flags; //XXX: Will die
 	struct list_head	list;
 	struct list_head	msg_head;
 	struct list_head	wq_head;
