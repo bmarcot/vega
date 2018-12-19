11340ec58a73480d97f7f9bd6b2212d1d4d31794
Benoit Marcot <juz4m@hotmail.com>
2018-12-19 14:52:20 +0100

    libc/include/mqueue.h libc/mqueue.c - mq_open with one or 2 parameters

--- libc/include/mqueue.h
+++ libc/include/mqueue.h
@@ -1,10 +1,17 @@
 #pragma once
 
+#include <stdarg.h>
 #include <asm/posix_types.h>
+#include <uapi/kernel/mqueue.h>
 
 typedef int mqd_t;
 
-mqd_t mq_open(const char *name, int oflag);
+mqd_t mq_open(const char *name, int oflag, ...);
+/* mqd_t mq_open(const char *name, int oflag); */
+/* mqd_t mq_open(const char *name, int oflag, mode_t mode, */
+/* 	struct mq_attr *attr); */
+
+
 int mq_send(mqd_t mqdes, const char *msg_ptr,
 	size_t msg_len, unsigned int msg_prio);
 ssize_t mq_receive(mqd_t mqdes, char *msg_ptr,
--- libc/mqueue.c
+++ libc/mqueue.c
@@ -6,11 +6,24 @@
 #include <asm/syscalls.h>
 #include <libvega/syscalls.h>
 
+#include <fcntl.h>    /* For O_* constants */
 #include <mqueue.h>
+#include <stddef.h>
 
-mqd_t mq_open(const char *name, int oflag)
+mqd_t mq_open(const char *name, int oflag, ...)
 {
-	return syscall(2, name, oflag, SYS_MQ_OPEN);
+	mode_t mode = 0;
+	struct mq_attr *attr = NULL;
+
+	if (oflag & O_CREAT) {
+		va_list ap;
+		va_start(ap, oflag);
+		mode = va_arg(ap, int);
+		attr = va_arg(ap, void *);
+		va_end(ap);
+	}
+
+	return syscall(4, name, oflag, mode, attr, SYS_MQ_OPEN);
 }
 
 int mq_send(mqd_t mqdes, const char *msg_ptr,
