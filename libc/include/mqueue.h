e267e2f0808b114ffc0abb0007bc015f57060e04
Benoit Marcot <juz4m@hotmail.com>
2019-03-20 08:10:45 +0100

    libc: Support posix prototype for mq_open

--- libc/include/mqueue.h
+++ libc/include/mqueue.h
@@ -1,10 +1,11 @@
 #pragma once
 
 #include <asm/posix_types.h>
+#include <uapi/kernel/mqueue.h>
 
 typedef int mqd_t;
 
-mqd_t mq_open(const char *name, int oflag);
+mqd_t mq_open(const char *name, int oflag, ...);
 int mq_send(mqd_t mqdes, const char *msg_ptr,
 	size_t msg_len, unsigned int msg_prio);
 ssize_t mq_receive(mqd_t mqdes, char *msg_ptr,
