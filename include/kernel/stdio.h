ac972cad7d9eb31f61bab15ed6f27b07386ea55c
Benoit Marcot <juz4m@hotmail.com>
2019-03-29 14:02:41 +0100

    misc: Declare sprintf-family functions in kernel header

--- include/kernel/stdio.h
+++ include/kernel/stdio.h
@@ -1,11 +1,15 @@
 /*
  * include/kernel/stdio.h
  *
- * Copyright (c) 2018 Benoit Marcot
+ * Copyright (c) 2018-2019 Benoit Marcot
  */
 
 #ifndef _KERNEL_STDIO_H
 #define _KERNEL_STDIO_H
+
+/* formatted output conversion */
+int sprintf(char *str, const char *format, ...);
+int snprintf(char *str, size_t size, const char *format, ...);
 
 int putchar(int c);
 int puts(const char *s);
