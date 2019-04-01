656437fa7d5856bd00dc2dc480883b2c606de336
Benoit Marcot <juz4m@hotmail.com>
2019-04-01 13:12:28 +0200

    libc: Declare open() syscall prototype

--- libc/include/unistd.h
+++ libc/include/unistd.h
@@ -5,6 +5,7 @@
 
 ssize_t read(int fd, void *buf, size_t count);
 ssize_t write(int fd, const void *buf, size_t count);
+int open(const char *pathname, int flags);
 int close(int fd);
 int stat(const char *pathname, struct stat *buf);
 int sleep(int secs);
